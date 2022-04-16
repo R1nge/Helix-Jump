f309babe13c722de4c01ecfa7fa71e5d533aa7e7
R1nge <r1ngegamedev@gmail.com>
2022-04-16 08:41:20 +0300

    Changed Camera Behaviour

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private float offset;
 
     private void LateUpdate()
     {
         if (!target) return;
         var pos = transform.position;
-        pos.y = target.position.y;
+        if(target.transform.position.y > pos.y) return;
+        pos.y = target.position.y + offset;
         transform.position = pos;
     }
 }
