157961739928645661850000c7fe790d4c55d06c
R1nge <r1ngegamedev@gmail.com>
2022-04-17 10:54:46 +0300

    Changed Camera Behaviour

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,14 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float offset;
+    private Vector3 _pos;
 
     private void LateUpdate()
     {
         if (!target) return;
-        var pos = transform.position;
-        if(target.transform.position.y > pos.y) return;
-        pos.y = target.position.y + offset;
-        transform.position = pos;
+        _pos = transform.position;
+        if (!(_pos.y >= target.transform.position.y + offset)) return;
+        _pos.y = target.transform.position.y + offset;
+        transform.position = _pos;
     }
 }
--- Assets/Scripts/Platforms/WinPlatform.cs
+++ Assets/Scripts/Platforms/WinPlatform.cs
@@ -1,10 +1,12 @@
 using Platforms;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinPlatform : Platform
 {
     protected override void OnCollision(Collision collision)
     {
         GameManager.WinGame();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
